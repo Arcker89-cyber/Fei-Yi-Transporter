51f143cef1f7bbd5be21e31b1a73d079d3994a63
Arcker89-cyber <customerarcker@gmail.com>
2025-12-16 17:23:28 +0700

    Create trips.js

---
+++ js/trips.js
@@ -0,0 +1,14 @@
+export const trips = [
+  {
+    id: "trip1",
+    route: "กรุงเทพ → โคราช",
+    time: "08:00",
+    seats: 10
+  },
+  {
+    id: "trip2",
+    route: "กรุงเทพ → โคราช",
+    time: "14:00",
+    seats: 8
+  }
+];
